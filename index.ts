723087912a24f212f8b5396c3ad1cccdb0250a52
Guilherme <eduardomartinspiangers@gmail.com>
2024-10-15 15:49:03 +0000

    Create simpleSort

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
-import { binaryShort } from "./binaryShort"
+import { binaryShort } from "./binarySearch"
+import { simpleShort } from "./shorts/simpleShort"
 
+// Binary searching
 const testList = [1,3,5,7,9]
+console.log("Binary search result:",binaryShort(testList, 5))
+console.log("-----------------------------")
 
-console.log("Binary sort result:",binaryShort(testList, 5))
+// Binary searching
+const testList2 = [1,6,2,7,3]
+console.log("simple sort result:",simpleShort(testList))
+console.log("-----------------------------")
---
+++ shorts/simpleShort.ts
@@ -0,0 +1,25 @@
+function getsmallestElement(arr: number[]){
+    let smallestElement = 0
+    let smallestElementIndex = 0
+    arr.forEach((element, index) => {
+        if(element < smallestElement){
+            smallestElement = element
+            smallestElementIndex = index
+        }
+    });
+    return smallestElementIndex
+}
+
+export function simpleShort(arr: number[]): number[]{
+    let array = [...arr]
+    const arrShorted = [] as number[]
+
+    arr.forEach(() => {
+        let smallestElementIndex = getsmallestElement(array)
+        const [lItem] = array.splice(smallestElementIndex, 1)
+        arrShorted.push(lItem)
+    });
+
+    return arrShorted
+}
+
