f9aa3cd9048f9dcddf13916eae24a0e842c917ca
Guilherme <eduardomartinspiangers@gmail.com>
2024-10-18 11:57:51 +0000

    Create Dijkstra algorithm

--- algoritmo_Dijkstra/index.ts
+++ algoritmo_Dijkstra/index.ts
@@ -18,3 +18,43 @@ const custos = {} as hashTable<number>
 custos["a"] = 6
 custos["b"] = 2
 custos["fim"] = Infinity
+
+const pais = {} as hashTable<string | undefined>
+
+pais["a"] = "inicio"
+pais["b"] = "inicio"
+pais["fim"] = undefined
+
+const processados = [] as string[]
+
+function acharMaisBarato(custos: hashTable<number>): string | undefined{
+    let maisBaratoPreco = Infinity
+    let maisBarato = undefined
+    for (let custoKey in custos){
+        if(custos[custoKey] < maisBaratoPreco && !processados.includes(custoKey)){
+            maisBaratoPreco = custos[custoKey]
+            maisBarato = custoKey
+        }
+    }
+    return maisBarato
+}
+
+function calculateSheapestTrade (){
+    let node = acharMaisBarato(custos)
+    while (node) {
+        const custo = custos[node]
+        const vizinhos = grafo[node]
+
+        for (let visinho in vizinhos) {
+            const novoCusto = custo + vizinhos[visinho]
+            if(novoCusto < custos[visinho]){
+                custos[visinho] = novoCusto
+                pais[visinho] = node
+            }
+        }
+        processados.push(node)
+        node = acharMaisBarato(custos)
+    }
+    console.log(pais)
+}
+calculateSheapestTrade()
