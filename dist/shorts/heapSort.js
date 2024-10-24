96170f0748bf93b54cdd2326c28722f10d3c23f4
Guilherme <eduardomartinspiangers@gmail.com>
2024-10-24 20:27:17 +0000

    HeapSort

---
+++ dist/shorts/heapSort.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function heap(list, length, i) {
+    let root = i;
+    let left = i * 2 + 1;
+    let right = i * 2 + 2;
+    if (left < length && list[root] < list[left]) {
+        root = left;
+    }
+    if (right < length && list[root] < list[right]) {
+        root = right;
+    }
+    if (root !== i) {
+        const helper = list[i];
+        list[i] = list[root];
+        list[root] = helper;
+        heap(list, length, root);
+    }
+}
+function heapSort(list) {
+    let length = list.length;
+    for (let i = Math.floor(length / 2 - 1); i >= 0; i--) {
+        heap(list, length, i);
+    }
+    console.log(array);
+    for (let n = length - 1; n > 0; n--) {
+        let helper = list[0];
+        list[0] = list[n];
+        list[n] = helper;
+        heap(list, n, 0);
+    }
+}
+const array = [3, 8, 2, 4, 1, 7, 10, 2, 20, 6, 9, 5];
+heapSort(array);
+console.log(array);
+//# sourceMappingURL=heapSort.js.map
--- dist/shorts/selectionSort.js
+++ dist/shorts/selectionSort.js
@@ -2,14 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function selectionSort(list) {
     for (let i = 0; i < list.length; i++) {
-        let smallest = list[i];
+        let smallestPosition = i;
         for (let j = i + 1; j < list.length; j++) {
-            if (list[j] < smallest) {
-                smallest = list[j];
-                list[j] = list[i];
-            }
+            if (list[j] < list[smallestPosition])
+                smallestPosition = j;
         }
-        list[i] = smallest;
+        const helper = list[i];
+        list[i] = list[smallestPosition];
+        list[smallestPosition] = helper;
     }
 }
 const array = [3, 8, 2, 4, 1, 7, 10, 2, 20, 6, 9, 5];
